cb4668e13d0534461547129e0729d6c1bb0505d6
BenjaminCristinelli <benjamin.c@dmconcept.fr>
2023-10-05 11:44:26 +0200

    ADD package to zoom on image and see them all on a task

--- components/Task.tsx
+++ components/Task.tsx
@@ -1,11 +1,12 @@
 import { Card } from "@rneui/base";
 import { CardDivider } from "@rneui/base/dist/Card/Card.Divider";
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { Text, View, TouchableOpacity, Image, Modal } from "react-native";
 import { deleteTask } from "../api/tasks";
 import { TaskStyle } from "../constants/Task";
 import { TaskType } from "../types/TaskType";
 import { Button } from "@rneui/themed";
+import ImageViewer from "react-native-image-zoom-viewer";
 
 const Task: React.FC<TaskType> = ({
   id,
@@ -17,9 +18,29 @@ const Task: React.FC<TaskType> = ({
 }) => {
   const taskId = id ? id : "";
   const colorTask = color ? color : "#999";
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
+
+  const openModal = (index) => {
+    setSelectedIndex(index);
+    setModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+
   return (
-    <Card containerStyle={[{ backgroundColor: colorTask}, TaskStyle.container]}>
-      <View style={{flexDirection: "row", justifyContent: "space-between", marginBottom: 5,}}>
+    <Card
+      containerStyle={[{ backgroundColor: colorTask }, TaskStyle.container]}
+    >
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          marginBottom: 5,
+        }}
+      >
         <Card.Title style={TaskStyle.titleTask}>{title}</Card.Title>
         <Button
           onPress={() => deleteTask(taskId)}
@@ -27,7 +48,7 @@ const Task: React.FC<TaskType> = ({
             name: "trash",
             type: "font-awesome",
             size: 15,
-            color: "#fff"
+            color: "#fff",
           }}
           accessibilityLabel="Learn more about this purple button"
           color="red"
@@ -42,7 +63,31 @@ const Task: React.FC<TaskType> = ({
         <Text style={TaskStyle.descriptionTitleTask}>
           Image de description :
         </Text>
-
+        <View style={{flexDirection: "row", flexWrap: "wrap"}}>
+          {images.map((image, index) => (
+            <TouchableOpacity
+              key={index}
+              onPress={() => openModal(index)}
+              style={{ marginRight: 10, marginBottom: 10,}}
+            >
+              <Image
+                source={{ uri: image }}
+                style={{ width: 115, height: 115 }}
+              />
+            </TouchableOpacity>
+          ))}
+        </View>
+        <Modal visible={isModalVisible} transparent={true}>
+          <Text style={{ color: "white" }}>
+            Swipper l'image vers le bas pour fermer la modal
+          </Text>
+          <ImageViewer
+            imageUrls={images.map((imageUrl) => ({ url: imageUrl }))}
+            index={selectedIndex}
+            enableSwipeDown
+            onSwipeDown={closeModal}
+          />
+        </Modal>
       </View>
     </Card>
   );
